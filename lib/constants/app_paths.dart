b376a3b88a34a61cc4ec0a3188a418dc1d11cba8
Mehmet Semih BABACAN <55881663+koltukutsu@users.noreply.github.com>
2022-11-15 23:38:19 +0300

    [Solve-Try]: probably because of the sound-player library's problem with reading the audio files' paths, I have to update the names of the songs

--- lib/constants/app_paths.dart
+++ lib/constants/app_paths.dart
@@ -2,117 +2,117 @@ class AppPaths {
   // // sesler
   // ninniler
   static const String ipekYumagim =
-      "sesler/ninniler/BABA Sesinden İpek Yumağım Ninnisi Bizim Ninniler.mp3";
+      "sesler/ninniler/ipek-yumagim.mp3";
   static const String ipekYumagimImage =
-      "gorseller/ninniler/baba sesinden ipek yumagi.png";
+      "gorseller/ninniler/baba-sesinden-ipek-yumagi.png";
 
   static const String banaBirMasalAnlatBaba =
-      "sesler/ninniler/Bana Bir Masal Anlat Baba.mp3";
+      "sesler/ninniler/bana-masal-anlat-baba.mp3";
   static const String banaBirMasalAnlatBabaImage =
-      "gorseller/ninniler/bana bir masal anlat baba.png";
+      "gorseller/ninniler/bana-bir-masal-anlat-baba.png";
 
   static const String ninnilerinMerdanesi =
-      "sesler/ninniler/BİZİM NİNNİLER  NİNNİLERİN MERDANESİ.mp3";
+      "sesler/ninniler/ninnilerin-merdanesi.mp3";
   static const String ninnilerinMerdanesiImage =
-      "gorseller/ninniler/ninniler medanesi.png";
+      "gorseller/ninniler/ninniler-medanesi.png";
 
   static const String gulecBebek =
-      "sesler/ninniler/Can Bebek Güleç Bebek Ninnisi.mp3";
+      "sesler/ninniler/gulec-bebek.mp3";
   static const String gulecBebekImage =
-      "gorseller/ninniler/gulec bebek ninnisi.png";
+      "gorseller/ninniler/gulec-bebek-ninnisi.png";
 
   static const String dandiniDandiniDastana =
-      "sesler/ninniler/Dandini-Dandini-Dastana.mp3";
+      "sesler/ninniler/dandini-dandini.mp3";
   static const String dandiniDandiniDastanaImage =
       "gorseller/ninniler/dandini.png";
 
-  static const String gelGelAnnecigim = "sesler/ninniler/Gel Gel Anneciğim.mp3";
+  static const String gelGelAnnecigim = "sesler/ninniler/gel-gel-annecigim.mp3";
   static const String gelGelAnnecigimImage =
-      "gorseller/ninniler/gel gel annecim.png";
+      "gorseller/ninniler/gel-gel-annecim.png";
 
-  static const String huAllah = "sesler/ninniler/Hu Hu Hu Allah Ninnisi.mp3";
-  static const String huAllahImage = "gorseller/ninniler/hu hu allah.png";
+  static const String huAllah = "sesler/ninniler/hu-hu-hu-Allah.mp3";
+  static const String huAllahImage = "gorseller/ninniler/hu-hu-allah.png";
 
-  static const String kayahanNinni = "sesler/ninniler/Kayahan Ninni.mp3";
+  static const String kayahanNinni = "sesler/ninniler/kayahan-ninni.mp3";
   static const String kayahanNinniImage =
-      "gorseller/ninniler/annesi onu sok severmis.png";
+      "gorseller/ninniler/annesi-onu-cok-severmis.png";
 
-  static const String kucukYavrum = "sesler/ninniler/Ozge-Oz-Kucuk-Yavrum.mp3";
-  static const String kucukYavrumImage = "gorseller/ninniler/kucuk yavrum.png";
+  static const String kucukYavrum = "sesler/ninniler/kucuk-yavrum.mp3";
+  static const String kucukYavrumImage = "gorseller/ninniler/kucuk-yavrum.png";
 
   static const String uyuYavrumYineSabahOluyor =
-      "sesler/ninniler/Ozge-Oz-Uyu-Yavrum-Yine-Sabah-Oluyor.mp3";
+      "sesler/ninniler/yavrum-yine-sabah.mp3";
   static const String uyuYavrumYineSabahOluyorImage =
-      "gorseller/ninniler/uyu yavrum yine sabah oluyor.png";
+      "gorseller/ninniler/uyu-yavrum-yine-sabah-oluyor.png";
 
   static const String pispisSesi =
-      "sesler/ninniler/Pis-Pis-Sesi-Bebek-Ninnisi.mp3";
-  static const String pispisSesiImage = "gorseller/ninniler/pis pis seni.png";
+      "sesler/ninniler/pis-pis-bebek.mp3";
+  static const String pispisSesiImage = "gorseller/ninniler/pis-pis-seni.png";
 
   static const String senBirGuzelMeleksin =
-      "sesler/ninniler/Sen bir güzel meleksin bebeğim.mp3";
+      "sesler/ninniler/sen-bir-guzel-meleksin.mp3";
   static const String senBirGuzelMeleksinImage =
-      "gorseller/ninniler/sen guzel bir meleksin.png";
+      "gorseller/ninniler/sen-guzel-bir-meleksin.png";
 
-  static const String uyusunDaBuyusun = "sesler/ninniler/Uyusun da Büyüsün.mp3";
+  static const String uyusunDaBuyusun = "sesler/ninniler/uyusun-da-buyusun.mp3";
   static const String uyusunDaBuyusunImage =
-      "gorseller/ninniler/uyusunda buyusun.png";
+      "gorseller/ninniler/uyusunda-buyusun.png";
 
   static const String fisFisKayikci =
-      "sesler/ninniler/Ninni-Bebek-Fis-Fis-Kayikci.mp3";
+      "sesler/ninniler/fis-fis-kayikci.mp3";
   static const String fisFisKayikciImage =
-      "gorseller/ninniler/fis fis kayikci.png";
+      "gorseller/ninniler/fisfiskayikci.png";
 
 // eglenceli sarkilar
   static const String aliBaba =
-      "sesler/eglenceli_sarkilar/ali babagin ciftligi.mp3";
+      "sesler/eglenceli_sarkilar/ali-babanin-ciftligi.mp3";
   static const String aliBabaImage =
-      "gorseller/eglenceli_sarkilar/ali baba ciftlik.png";
+      "gorseller/eglenceli_sarkilar/ali-baba.png";
 
-  static const String aramsamsam = "sesler/eglenceli_sarkilar/Aramsamsam.mp3";
+  static const String aramsamsam = "sesler/eglenceli_sarkilar/aramsamsam.mp3";
   static const String aramsamsamImage =
       "gorseller/eglenceli_sarkilar/aramsamsam.png";
 
-  static const String ariVizViz = "sesler/eglenceli_sarkilar/Arı Vız Vız.mp3";
+  static const String ariVizViz = "sesler/eglenceli_sarkilar/ari-viz-viz-viz.mp3";
   static const String ariVizVizImage =
-      "gorseller/eglenceli_sarkilar/ari viz viz viz.png";
+      "gorseller/eglenceli_sarkilar/ari-viz-viz.png";
 
   static const String arkadasimEsek =
-      "sesler/eglenceli_sarkilar/arkadaşım eşşek.mp3";
+      "sesler/eglenceli_sarkilar/arkadasim-esek.mp3";
   static const String arkadasimEsekImage =
-      "gorseller/eglenceli_sarkilar/arkadasim esek.png";
+      "gorseller/eglenceli_sarkilar/arkadasim-esek.png";
 
   static const String kedilerHepMiyav =
-      "sesler/eglenceli_sarkilar/Kediler Hep Miyav Der  Hayvan Seslerini Öğreten Bebek Şarkısı.mp3";
+      "sesler/eglenceli_sarkilar/kediler-hep-miyav-der.mp3";
   static const String kedilerHepMiyavImage =
-      "gorseller/eglenceli_sarkilar/kediler hep miyaav dedi.png";
+      "gorseller/eglenceli_sarkilar/kediler-miyav.png";
 
   static const String kirmiziBalikGolde =
-      "sesler/eglenceli_sarkilar/kirmizi-balik-sevimli-dostlar-ile.mp3";
+      "sesler/eglenceli_sarkilar/kirmizi-balik-sevimli.mp3";
   static const String kirmiziBalikGoldeImage =
-      "gorseller/eglenceli_sarkilar/kirmizi balik golde.png";
+      "gorseller/eglenceli_sarkilar/kirmizi-balik-golde.png";
 
   static const String meyveler =
-      "sesler/eglenceli_sarkilar/meyveler-sarkisi-cocuklar-icin-meyveler.mp3";
+      "sesler/eglenceli_sarkilar/meyveler.mp3";
   static const String meyvelerImage =
       "gorseller/eglenceli_sarkilar/meyveler.png";
 
   static const String yagmurYagiyor =
-      "sesler/eglenceli_sarkilar/Yağmur Yağıyor Seller Akıyor Çocuk Şarkısı ve Eğlenceli Çocuk Şarkılari.mp3";
+      "sesler/eglenceli_sarkilar/yagmur-yagiyor.mp3";
   static const String yagmurYagiyorImage =
-      "gorseller/eglenceli_sarkilar/yagmur yagiyor.png";
+      "gorseller/eglenceli_sarkilar/yagmur-yagiyor.png";
 
   // beyaz gurultuler
-  static const String balinaSesi = "sesler/beyaz_gurultu/balina sesi.mp3";
+  static const String balinaSesi = "sesler/beyaz_gurultu/balina-sesi.mp3";
   static const String balinaSesiImage = "gorseller/beyaz_gurultu/balina.png";
 
   static const String dalgaSesi = "sesler/beyaz_gurultu/dalga-sesleri.mp3";
-  static const String dalgaSesiImage = "gorseller/beyaz_gurultu/dalga sesi.png";
+  static const String dalgaSesiImage = "gorseller/beyaz_gurultu/dalga-sesi.png";
 
   static const String elektrikliSupurgeSesi =
-      "sesler/beyaz_gurultu/Klasik Elektrikli Süpürge Sesi  1 saat.mp3";
+      "sesler/beyaz_gurultu/elektrikli-supurge.mp3";
   static const String elektrikliSupurgeSesiImage =
-      "gorseller/beyaz_gurultu/elektrik supurgesi.png";
+      "gorseller/beyaz_gurultu/elektrik-supurgesi.png";
 
   static const String ruzgarUgultuSesi =
       "sesler/beyaz_gurultu/ruzgar-ugultu-sesi.mp3";
@@ -120,7 +120,7 @@ class AppPaths {
       "gorseller/beyaz_gurultu/bulut.png";
 
   static const String sacKurutmaMakinesi =
-      "sesler/beyaz_gurultu/Sac-Kurutma-Makinesi-Sesi.mp3";
+      "sesler/beyaz_gurultu/sac-kurutma.mp3";
   static const String sacKurutmaMakinesiImage =
-      "gorseller/beyaz_gurultu/sac kurutma.png";
+      "gorseller/beyaz_gurultu/sac-kurutmasi.png";
 }
